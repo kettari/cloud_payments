f725a210ff23732e19b4f52b15537b43d080767c
Andy <ant@kaula.ru>
2017-06-26 19:35:24 +0300

    Fixes, fixes, fixes

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -2,11 +2,11 @@
 
 return array(
   'name'        => 'CloudPayments',
-  'description' => 'Платежная система <a href="https://cloudpayments.ru/">CloudPayments</a>',
+  'description' => 'Платежная система <a href="https://cloudpayments.ru/">CloudPayments</a> (Visa, MasterCard, МИР)',
   'icon'        => 'img/cloudpayments16.png',
   'logo'        => 'img/cloudpayments.png',
   'vendor'      => '1084205',
-  'version'     => '1.0',
+  'version'     => '1.0.4',
   'locale'      => array('ru_RU',),
   'type'        => waPayment::TYPE_ONLINE,
 );
