0a2192976149ac86be1be356e3dad2461541622a
Andy <ant@kaula.ru>
2017-06-26 19:51:41 +0300

    Updated version

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -6,7 +6,7 @@ return array(
   'icon'        => 'img/cloudpayments16.png',
   'logo'        => 'img/cloudpayments.png',
   'vendor'      => '1084205',
-  'version'     => '1.0.4',
+  'version'     => '1.0.5',
   'locale'      => array('ru_RU',),
   'type'        => waPayment::TYPE_ONLINE,
 );
