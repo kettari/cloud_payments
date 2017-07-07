0f074211c798f19e6981f119e8971563b2e6e542
Andy <ant@kaula.ru>
2017-07-07 17:53:37 +0300

    cloud_payments-1 Add target=_blank to the link to CloudPayments in the plugin description

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -2,7 +2,7 @@
 
 return array(
   'name'        => 'CloudPayments',
-  'description' => 'Платежная система <a href="https://cloudpayments.ru/">CloudPayments</a> (Visa, MasterCard, МИР)',
+  'description' => 'Платежная система <a href="https://cloudpayments.ru/" target="_blank">CloudPayments</a> (Visa, MasterCard, МИР)',
   'icon'        => 'img/cloudpayments16.png',
   'logo'        => 'img/cloudpayments.png',
   'vendor'      => '1084205',
