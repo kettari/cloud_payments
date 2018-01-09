e436005bb842432a5326f3a59631af9fc1dbd19c
Andy <ant@kaula.ru>
2018-01-09 21:02:26 +0300

    #11 Дополнить README.md, внести параметры и changelog, исправить версию

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -6,7 +6,7 @@ return array(
   'icon'        => 'img/cloudpayments16.png',
   'logo'        => 'img/cloudpayments.png',
   'vendor'      => '1084205',
-  'version'     => '1.1.0-beta.1',
+  'version'     => '1.1.0',
   'locale'      => array('ru_RU',),
   'type'        => waPayment::TYPE_ONLINE,
 );
